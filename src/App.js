bc9c64da71530ae111902ccf22bf891e82b394ae
jbelagamba <jaquelinebdemoraes@gmail.com>
2022-03-23 20:07:15 -0300

    ajustes bootstrap

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import { Routes, Route } from 'react-router-dom';
+import './index.scss';
+
 import Login from './pages/Login';
 import Clientes from './pages/Clientes';
 import Header from './components/Header';
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,16 +1,16 @@
-import { Container, Button } from 'react-bootstrap';
+import { Container } from 'react-bootstrap';
 import Navbar from 'react-bootstrap/Navbar';
 
 function Header() {
   return (
-    <Navbar bg="dark" variant="dark">
-        <Container>
-          <Navbar.Brand href="/clientes">Instaltec</Navbar.Brand>
-          <Navbar.Text>
-            Contato: <a href="tel:981093897">981093897</a>
-          </Navbar.Text>
-        </Container>
-      </Navbar>
+    <Navbar bg="primary" variant="dark">
+      <Container>
+        <Navbar.Brand href="/clientes">Instaltec</Navbar.Brand>
+        <Navbar.Text>
+          Contato: <a href="tel:981093897">981093897</a>
+        </Navbar.Text>
+      </Container>
+    </Navbar>
   );
 }
 export default Header;
