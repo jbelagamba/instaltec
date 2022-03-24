21085dd53d19e2949211744e09a7683fe540d976
jbelagamba <jaquelinebdemoraes@gmail.com>
2022-03-24 04:01:17 -0300

    modal de cadastro

--- src/pages/Clientes/index.js
+++ src/pages/Clientes/index.js
@@ -1,9 +1,22 @@
-import { Layout, PageHeader, Divider, Table, Button } from 'antd';
+import React, { useState } from 'react';
+import {
+  Layout,
+  PageHeader,
+  Divider,
+  Table,
+  Button,
+  Modal,
+  Form,
+  Input,
+} from 'antd';
 import { PlusOutlined } from '@ant-design/icons';
 import { columns } from './constants';
 const { Content } = Layout;
 
 function Clientes() {
+  const [loading, setLoading] = useState(false);
+  const [modalCadastro, setModalCadastro] = useState(false);
+
   const data = [
     {
       key: '1',
@@ -71,13 +84,22 @@ function Clientes() {
     },
   ];
 
+  const onFinish = (values) => {
+    setLoading(true);
+    console.log('Success:', values);
+  };
+
   return (
     <Content className="container whiteBox">
       <PageHeader
         title="Clientes"
         className="pageHeader"
         extra={
-          <Button type="primary" icon={<PlusOutlined />}>
+          <Button
+            type="primary"
+            icon={<PlusOutlined />}
+            onClick={() => setModalCadastro(true)}
+          >
             Cadastrar cliente
           </Button>
         }
@@ -86,6 +108,63 @@ function Clientes() {
       <Divider />
 
       <Table columns={columns} dataSource={data} />
+
+      <Modal
+        title="Cadastro de cliente"
+        visible={modalCadastro}
+        footer={false}
+        onCancel={() => setModalCadastro(false)}
+      >
+        <Form
+          name="novoCliente"
+          onFinish={onFinish}
+          autoComplete="off"
+          layout="vertical"
+        >
+          <Form.Item
+            label="Código cliente"
+            name="codigo_cliente"
+            rules={[
+              { required: true, message: 'Informe o Código Nome cliente!' },
+            ]}
+          >
+            <Input />
+          </Form.Item>
+          <Form.Item
+            label="Nome cliente"
+            name="nome"
+            rules={[{ required: true, message: 'Informe o Nome cliente!' }]}
+          >
+            <Input />
+          </Form.Item>
+          <Form.Item
+            label="Representante"
+            name="representante"
+            rules={[{ required: true, message: 'Informe o Representante!' }]}
+          >
+            <Input />
+          </Form.Item>
+          <Form.Item
+            label="Email"
+            name="email"
+            rules={[{ required: true, message: 'Informe o Email!' }]}
+          >
+            <Input />
+          </Form.Item>
+          <Form.Item
+            label="Telefone"
+            name="telefone"
+            rules={[{ required: true, message: 'Informe o Telefone!' }]}
+          >
+            <Input />
+          </Form.Item>
+          <Form.Item>
+            <Button type="primary" htmlType="submit" loading={loading}>
+              Cadastrar
+            </Button>
+          </Form.Item>
+        </Form>
+      </Modal>
     </Content>
   );
 }
