91e0da0e3f8c32a698ba6963e33828a768d8ac27
jbelagamba <jaquelinebdemoraes@gmail.com>
2022-03-31 03:39:58 -0300

    qwerqwer

---
+++ src/pages/Pedido/constants.js
@@ -0,0 +1,37 @@
+export const camposFormulario = [
+  {
+    type: 'select',
+    label: 'Abas',
+    name: 'abas',
+    options: [
+      { label: 'Aba1', value: 1 },
+      { label: 'Aba2', value: 2 },
+      { label: 'Aba3', value: 3 },
+    ],
+  },
+  {
+    type: 'textArea',
+    label: 'descricao',
+    name: 'descricao',
+  },
+  {
+    type: 'text',
+    label: 'Prazo de Execução',
+    name: 'prazo_execucao',
+  },
+  {
+    type: 'text',
+    label: 'Data Limite',
+    name: 'data_limite',
+  },
+  {
+    type: 'text',
+    label: 'Materiais',
+    name: 'data_limite',
+  },
+  {
+    type: 'text',
+    label: 'Data Limite',
+    name: 'Ferramenta',
+  },
+];
